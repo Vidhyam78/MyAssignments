bc9ea00ef5e368563c5fe9f96a00f8cf74ddde14
Vidhyam78 <sreevidhya.mohan@gmail.com>
2025-12-06 20:36:59 -0500

    Add files via upload
    Day1 Assignment1- PrimitiveDataTypes

---
+++ PrimitiveDataTypes.js
@@ -0,0 +1,12 @@
+let firstName="Sreevidhya"
+let companyName= "NVIDIA"
+let cellnum = 4034445555
+let isAutomation = true
+let hasPlaywright = undefined
+
+console.log( "Firstname is : " + firstName  +"\n" + "companyName is " +companyName + "\n" +"cellnum is "
+     +cellnum +"\n" + "Automatable is : " +isAutomation + "\n" + "Has Playright : " + hasPlaywright  )
+
+console.log("Datatype of firstName is " +typeof (firstName) +"\n" + "Datatype of CompanyName is " + typeof (companyName)
+ + "\n" + "Datatype of cellnum is " + typeof (cellnum) +"\n" + "Datatype of isAutomation is " + typeof (isAutomation) +"\n" + "Datatype of hasPlayright is " + typeof (hasPlaywright))
+
